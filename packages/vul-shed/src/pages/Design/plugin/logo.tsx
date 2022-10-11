f037898d1eed39b517e43146e2c77233655a7087
InternationSi <sam2020888@outlook.com>
2022-10-11 23:49:22 +0800

    拆分组件

--- packages/vul-shed/src/pages/Design/plugin/logo.tsx
+++ packages/vul-shed/src/pages/Design/plugin/logo.tsx
@@ -2,10 +2,12 @@
  * @Author: sfy
  * @Date: 2022-10-11 23:39:18
  * @LastEditors: sfy
- * @LastEditTime: 2022-10-11 23:39:19
+ * @LastEditTime: 2022-10-11 23:49:15
  * @FilePath: /vulture/packages/vul-shed/src/pages/Design/plugin/logo.tsx
  * @Description: update here
  */
+import React, { useEffect } from 'react'
+
 const Logo: React.FC = () => (
   <div style={{ display: 'flex', alignItems: 'center', fontSize: 14 }}>
     <div>Vulture</div>
