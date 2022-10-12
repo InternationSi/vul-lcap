6b55eee8b95acc09eefa254c5b122b981c200f9d
InternationSi <sam2020888@outlook.com>
2022-10-12 22:52:44 +0800

    11

--- packages/vul-shed/src/pages/Design/sandbox.tsx
+++ packages/vul-shed/src/pages/Design/sandbox.tsx
@@ -2,12 +2,12 @@
  * @Author: sfy
  * @Date: 2022-10-05 20:07:35
  * @LastEditors: sfy
- * @LastEditTime: 2022-10-11 23:31:31
+ * @LastEditTime: 2022-10-12 22:51:53
  * @FilePath: /vulture/packages/vul-shed/src/pages/Design/sandbox.tsx
  * @Description: update here
  */
 import React from 'react'
-import { Content } from './content'
+import { Content } from './material'
 import { renderSandboxContent } from '@designable/react-sandbox'
 import './theme.less'
 
