6d97c043a6a4c7d82ccf63041d3aa5b16e101542
panyanan <1194229129@qq.com>
2022-10-18 23:13:15 +0800

    表格

---
+++ packages/vul-shed/src/.umi/core/history.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { createBrowserHistory, History } from '/Users/yananpan/Desktop/0905-vue3/vulture/packages/vul-shed/node_modules/@umijs/runtime';
+
+let options = {
+  "basename": "/"
+};
+if ((<any>window).routerBase) {
+  options.basename = (<any>window).routerBase;
+}
+
+// remove initial history because of ssr
+let history: History = process.env.__IS_SERVER ? null : createBrowserHistory(options);
+export const createHistory = (hotReload = false) => {
+  if (!hotReload) {
+    history = createBrowserHistory(options);
+  }
+
+  return history;
+};
+
+export { history };
---
+++ packages/vul-shed/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/yananpan/Desktop/0905-vue3/vulture/packages/vul-shed/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
