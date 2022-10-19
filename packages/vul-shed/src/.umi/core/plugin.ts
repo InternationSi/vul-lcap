d8818590e79c9691331f3d1262e8d80d97c97483
InternationSi <sam2020888@outlook.com>
2022-10-19 22:05:34 +0800

    fix

--- packages/vul-shed/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/yananpan/Desktop/0905-vue3/vulture/packages/vul-shed/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- packages/vul-shed/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/yananpan/Desktop/0905-vue3/vulture/packages/vul-shed/node_modules/react-helmet';
