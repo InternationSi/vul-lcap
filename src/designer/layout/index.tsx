500d84b0706c7d9ebf204c4935cfc90882484917
InternationSi <sam2020888@outlook.com>
2022-10-22 23:07:46 +0800

    fix

--- src/designer/layout/index.tsx
+++ src/designer/layout/index.tsx
@@ -2,21 +2,29 @@
  * @Author: sfy
  * @Date: 2022-10-22 21:34:52
  * @LastEditors: sfy
- * @LastEditTime: 2022-10-22 21:38:44
+ * @LastEditTime: 2022-10-22 22:51:22
  * @FilePath: /vulture/src/designer/layout/index.tsx
  * @Description: update here
  */
 import { defineComponent, ref } from "vue";
+import Grid from "../grid";
+import leftDash from "./components/leftDash";
+import "./index.less";
 
 export default defineComponent({
+  components: { Grid, Notification, leftDash },
   setup() {
     return () => (
       <div class="common-layout">
         <el-container>
-          <el-header>Header</el-header>
-          <el-container>
-            <el-aside width="200px">Aside</el-aside>
-            <el-main>Main</el-main>
+          <el-header class="main-head"></el-header>
+          <el-container class="main-layout">
+            <el-aside width="70px">
+              <leftDash />
+            </el-aside>
+            <el-main>
+              <Grid />
+            </el-main>
           </el-container>
         </el-container>
       </div>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,14 +2,19 @@
  * @Author: sfy
  * @Date: 2022-10-13 11:46:06
  * @LastEditors: sfy
- * @LastEditTime: 2022-10-15 15:06:07
- * @FilePath: /vulture/packages/vul-admin/src/shims-vue.d.ts
+ * @LastEditTime: 2022-10-22 22:30:38
+ * @FilePath: /vulture/src/shims-vue.d.ts
  * @Description: update here
  */
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
-declare module 'vue-grid-layout'
+declare module "vue-grid-layout";
+
+declare module "*.module.less" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
