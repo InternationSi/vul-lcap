e0e5362cff899f20a82fdf61f207f5902fedb5f3
why <1789244998@qq.com>
2022-10-30 00:13:52 +0800

    icon

--- src/designer/grid/index.tsx
+++ src/designer/grid/index.tsx
@@ -6,53 +6,57 @@
  * @FilePath: /vulture/src/designer/grid/index.tsx
  * @Description: update here
  */
-import { defineComponent, ref, watch, onUnmounted } from "vue";
-import useAddGridItem from "./effect/useAddGridItem";
-import useCreateConfig from './effect/useCreateConfig'
-import { nanoid } from 'nanoid'
-import GridItemList from "./components/GridItemList";
-import "./index.moduel.less";
+import { defineComponent, ref, watch, onUnmounted } from 'vue';
+import useAddGridItem from './effect/useAddGridItem';
+import useCreateConfig from './effect/useCreateConfig';
+import { nanoid } from 'nanoid';
+import GridItemList from './components/GridItemList';
+import Operation from './components/Operation';
+import './index.moduel.less';
 
 export default defineComponent({
   setup() {
     const { itemInfo } = useAddGridItem();
-    const layout = ref([{ x: 0, y: 0, w: 2, h: 3, i: "0",}]);
+    const layout = ref([{ x: 0, y: 0, w: 2, h: 3, i: '0' }]);
 
     watch(
       () => itemInfo.value,
       (value) => {
-        layout.value.push(useCreateConfig({
-          type: value,
-        }));
+        layout.value.push(
+          useCreateConfig({
+            type: value,
+          })
+        );
       }
     );
 
     return () => (
-        <grid-layout
-          layout={layout.value}
-          col-num={12}
-          row-height={30}
-          is-draggable={true}
-          is-resizable={true}
-          vertical-compact={true}
-          use-css-transforms={true}
-        >
-          {layout.value.map((item: any, index: number) => {
-            return (
-              <grid-item
-                key={index}
-                static={item.static}
-                x={item.x}
-                y={item.y}
-                w={item.w}
-                h={item.h}
-                i={item.i}
-              >
-                <GridItemList config={item} />
-              </grid-item>
-            );
-          })}
-        </grid-layout>
+      <grid-layout
+        layout={layout.value}
+        col-num={12}
+        row-height={30}
+        is-draggable={true}
+        is-resizable={true}
+        vertical-compact={true}
+        use-css-transforms={true}
+      >
+        {layout.value.map((item: any, index: number) => {
+          return (
+            <grid-item
+              key={index}
+              static={item.static}
+              x={item.x}
+              y={item.y}
+              w={item.w}
+              h={item.h}
+              i={item.i}
+            >
+              <GridItemList config={item} />
+              <Operation/>
+            </grid-item>
+          );
+        })}
+      </grid-layout>
     );
-  }
+  },
 });
