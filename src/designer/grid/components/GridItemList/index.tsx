d1ff339853d8912caa221322669bc3254947544b
why <1789244998@qq.com>
2022-10-31 22:46:14 +0800

    operation

--- src/designer/grid/components/GridItemList/index.tsx
+++ src/designer/grid/components/GridItemList/index.tsx
@@ -9,31 +9,50 @@
 import { defineComponent, ref, watch, onUnmounted } from "vue";
 import Spacing from "../Spacing";
 import TabContainer from "../TabContainer";
+import Operation from '../../../../components/Operation';
 export default defineComponent({
   props:{
     config: {
       type: Object as any,
       require: true
-    }
+    },
+    itemIndex: {
+      type: Number as any,
+      require: true,
+    },
+    handleDelete: {
+      type: Function as any,
+      required: true,
+    },
+    handleEdit: {
+      type: Function as any,
+      required: true,
+    },
   },
   setup(props) {
-    const renderComponents = () => {
-      const { config } = props
-
-      console.log(config, '----');
-       
+    const { config,itemIndex } = props
+    
+    const renderComponents = () => {       
       if(config.type === 'container') {
         return <TabContainer />
       }else {
         return <Spacing />
       }
     }
+    const deleteItem = (value:number)=>{
+      props.handleDelete(value)
+    }
+    const editItem = (value:any)=>{
+      props.handleEdit(value)
+    }
 
     return () => (
       <>
         {
           renderComponents()
+          
         }
+        <Operation  itemIndex = {itemIndex} handleDelete = {deleteItem} handleEdit={editItem}/>
       </>
     );
   }
--- src/designer/grid/index.tsx
+++ src/designer/grid/index.tsx
@@ -6,19 +6,41 @@
  * @FilePath: /vulture/src/designer/grid/index.tsx
  * @Description: update here
  */
-import { defineComponent, ref, watch, onUnmounted } from 'vue';
+import { defineComponent, ref, reactive, watch, onUnmounted } from 'vue';
 import useAddGridItem from './effect/useAddGridItem';
 import useCreateConfig from './effect/useCreateConfig';
 import { nanoid } from 'nanoid';
 import GridItemList from './components/GridItemList';
-import Operation from './components/Operation';
 import './index.moduel.less';
+import { number } from 'echarts/core';
 
 export default defineComponent({
   setup() {
     const { itemInfo } = useAddGridItem();
     const layout = ref([{ x: 0, y: 0, w: 2, h: 3, i: '0' }]);
-
+    let dialogVisible = ref(false);
+    let form = reactive({
+      x: '',
+      y: '',
+      w: '',
+      h: '',
+      i: '',
+    });
+    let index = ref(0);
+    const deleteItem = (value: number) => {
+      index.value = value;
+      layout.value = layout.value.filter((item, index) => {
+        return index !== value;
+      });
+    };
+    const editItem = () => {
+      layout.value[index.value].x = Number(form.x);
+      layout.value[index.value].y = Number(form.y);
+      layout.value[index.value].w = Number(form.w);
+      layout.value[index.value].h = Number(form.h);
+      layout.value[index.value].i = form.i;
+      dialogVisible.value = false;
+    };
     watch(
       () => itemInfo.value,
       (value) => {
@@ -51,11 +73,48 @@ export default defineComponent({
               h={item.h}
               i={item.i}
             >
-              <GridItemList config={item} />
-              <Operation/>
+              <GridItemList
+                config={item}
+                itemIndex={index}
+                handleDelete={deleteItem}
+                handleEdit={(value: boolean) => {
+                  dialogVisible.value = value;
+                }}
+              />
             </grid-item>
           );
         })}
+        <el-dialog v-model={dialogVisible.value} title="编辑单位" width="30%">
+          <el-form model={form}>
+            <el-form-item label="x:">
+              <el-input v-model={form.x} />
+            </el-form-item>
+            <el-form-item label="y:">
+              <el-input v-model={form.y} />
+            </el-form-item>
+            <el-form-item label="w:">
+              <el-input v-model={form.w} />
+            </el-form-item>
+            <el-form-item label="h:">
+              <el-input v-model={form.h} />
+            </el-form-item>
+            <el-form-item label="i:">
+              <el-input v-model={form.i} />
+            </el-form-item>
+          </el-form>
+          <span class="dialog-footer">
+            <el-button
+              onClick={() => {
+                dialogVisible.value = false;
+              }}
+            >
+              取消
+            </el-button>
+            <el-button type="primary" onClick={editItem}>
+              保存
+            </el-button>
+          </span>
+        </el-dialog>
       </grid-layout>
     );
   },
