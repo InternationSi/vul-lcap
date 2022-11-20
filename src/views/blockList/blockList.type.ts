66f5f2740792d262d5ed8de97093b84cc29f9c4b
panyanan <1194229129@qq.com>
2022-11-20 23:41:29 +0800

    新接口

--- src/views/blockList/blockList.type.ts
+++ src/views/blockList/blockList.type.ts
@@ -2,9 +2,10 @@ export interface selectItem {
   namespace_label: string;
   namespace_name: string;
 }
-export interface moduleName {
+export interface ModuleName {
   id: string;
   module_key: string;
   module_name: string;
   namespace_id: string;
+  category: string;
 }
