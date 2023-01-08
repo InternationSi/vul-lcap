85607021b6c7933a007ffb06976fe3ec80b65f02
xiaosi <sam2020888@outlook.com>
2023-01-08 22:12:23 +0800

    feat:接入tauri

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: sfy
  * @Date: 2023-01-06 15:50:48
  * @LastEditors: sfy
- * @LastEditTime: 2023-01-06 15:58:20
+ * @LastEditTime: 2023-01-08 17:44:08
  * @FilePath: /vulture/vite.config.ts
  * @Description: update here
  */
@@ -18,8 +18,10 @@ export default defineConfig({
   plugins: [vue(), vueJsx({
     // options are passed on to @vue/babel-plugin-jsx
   })],
+  clearScreen: false,
   server: {
     port: 8080,
+    strictPort: true,
     proxy: {
       '/api': { // 匹配所有以'/api1'开头的请求路径
         target: "http://112.124.8.236:8888", // 代理目标的基础路径
@@ -32,5 +34,14 @@ export default defineConfig({
     alias: {
       "@": resolve(__dirname, "/src"),
     },
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 })
